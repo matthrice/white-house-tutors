cbb35c5fc9b31bc44134f8a4fdb14bccd3fb4b61
matthrice <mattrice@umich.edu>
2017-12-23 18:43:02 -0600

    last commit 12/24

--- src/components/tutor-info.js
+++ src/components/tutor-info.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 const TutorInfoBlock = (props) => (
     <div class="tutor-info-block">
-        <span>
-            <div class="content">
-                <img class="tutor-photo" src={props.imgsrc} alt="Avatar"></img>
-                <div class="tutor-description">
-                    <h1>{props.name}</h1>
-                    <p>{props.description}</p>
-                </div>
+        <div class="content">
+            <img class="tutor-photo" src={props.imgsrc} alt="Avatar"></img>
+            <div class="tutor-description">
+                <h1>{props.name}</h1>
+                <p>{props.description}</p>
             </div>
-        </span>
+        </div>
     </div>
 )
 
