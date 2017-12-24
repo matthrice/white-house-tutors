3327019e71e464e3f2c506a1d01259b894206a56
matthrice <mattrice@umich.edu>
2017-12-24 11:46:40 -0600

    tutor page tests

--- src/pages/tutors.js
+++ src/pages/tutors.js
@@ -5,7 +5,16 @@ const TutorPage = () => (
     <div class="main">
         <Header />
         <TitleBlock title="Tutors" />
-        <TutorInfoBlock imgsrc="http://eecs.umich.edu/eecs/about/articles/2016/paoletti200.jpg" name="dave" description="hi" />
+        <TutorInfoBlock 
+            imgsrc="http://eecs.umich.edu/eecs/about/articles/2016/paoletti200.jpg"
+            name="Dave"
+            description="Beast"
+        />
+        <TutorInfoBlock
+            imgsrc="https://www.biography.com/.image/t_share/MTIwNjA4NjMzODg2NTc0MDky/abraham-lincoln-9382540-2-402.jpg "
+            name="Abraham"
+            description="Also a beast"
+        />
     </div>
 );
 
